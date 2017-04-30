bedb68b6eb392d84d3a73d25a5e6a9aa52b96cf3
Phil Cockfield <phil@cockfield.net>
2017-04-30 15:03:51 +1200

    Fix lint

--- src/common/util.file.test.ts
+++ src/common/util.file.test.ts
@@ -5,8 +5,8 @@ import * as file from './util.file';
 
 describe('file.yaml', () => {
   it('loads a yaml file', async () => {
-    const path = fsPath.join(__dirname, '../examples/sample.yaml')
-    const result = await file.yaml<any>(path)
+    const path = fsPath.join(__dirname, '../examples/sample.yaml');
+    const result = await file.yaml<any>(path);
     expect(result.array).to.be.an.instanceof(Array);
     expect(result.array.length).to.equal(3);
   });
