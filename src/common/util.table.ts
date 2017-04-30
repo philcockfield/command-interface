5b6f75045c8187303d2791ae23aab451a8e47b7e
Phil Cockfield <phil@cockfield.net>
2017-04-30 12:38:37 +1200

    Documentation.

--- src/common/util.table.ts
+++ src/common/util.table.ts
@@ -13,11 +13,22 @@ export function table(head: Array<string | undefined> = []) {
   head = compact(head);
   const t = new Table({ head });
   const api = {
-    add(...rows: Array<string | undefined>) {
-      t.push(rows.map((row) => row === undefined ? '' : row));
+    /**
+     * Adds a new row to the table.
+     */
+    add(...columns: Array<string | undefined>) {
+      t.push(columns.map((row) => row === undefined ? '' : row));
       return api;
     },
+
+    /**
+     * Converts the table to a string.
+     */
     toString() { return t.toString(); },
+
+    /**
+     * Logs the table to the console.
+     */
     log() { log.info(api.toString()); return api; },
   };
   return api;
