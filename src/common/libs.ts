08d5782c10d72253f67aa5238ef78f6d8d30447b
Phil Cockfield <phil@cockfield.net>
2017-04-30 15:11:33 +1200

    add debounce

--- src/common/libs.ts
+++ src/common/libs.ts
@@ -4,7 +4,8 @@ import * as fsPath from 'path';
 import * as jsYaml from 'js-yaml';
 import * as chokidar from 'chokidar';
 import * as moment from 'moment';
+import { debounce } from 'lodash';
 
-export { R, fs, fsPath, chokidar, jsYaml, moment };
+export { R, fs, fsPath, chokidar, jsYaml, moment, debounce };
 export { Subject, Observable } from 'rxjs';
 export { log } from 'js-util-log';
